213a947ee2bc1ca7008ae4bab8b6067fecd14c78
RAFA-PC\Rafael <rafaelcon96@gmail.com>
2019-12-19 10:31:10 +0200

    implemented firebase utils, including ability to store authenticated users into fire store.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import HomePage from './pages/homePage/homePage';
 import ShopPage from './pages/shop/shop';
 import Header from './components/header/header';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 import './App.css';
 
@@ -21,9 +21,25 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+     if (userAuth) { 
+      const userRef = await createUserProfileDocument(userAuth);
+
+      userRef.onSnapshot(snapShot => {
+        this.setState({
+          currentUser: {
+            id: snapShot.id,
+            ...snapShot.data()
+          }
+        });
+        console.log(this.state);
+      });
+      
+     } else { //if user log off
+       this.setState({currentUser: userAuth});
+     }
+      
+     
     })
   };
 
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
-//import './sign-in-and-sign-up.styles.scss';
-import SignIn from '../../components/sign-in/sign-in'
+import './sign-in-and-sign-up.styles.scss';
+import SignIn from '../../components/sign-in/sign-in';
+import SignUp from '../../components/sign-up/sign-up';
 
 
 const SignInAndSignUp = () =>(
-<div className='sign-in-and-sign-up'><SignIn /></div>
+    <div className='sign-in-and-sign-up'>
+        <SignIn />
+        <SignUp />
+    </div>
 
-);
+    );
 
 export default SignInAndSignUp;
   
